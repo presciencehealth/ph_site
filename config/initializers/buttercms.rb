3d655960ed19b7dfd613ffc0994ade8cdfda20b5
presciencehealth <hiro@presciencehealth.io>
2018-12-30 16:23:12 +0000

    Additional changes to buttercms API token

--- config/initializers/buttercms.rb
+++ config/initializers/buttercms.rb
@@ -1,8 +1,8 @@
-require 'buttercms-rails'
+require 'buttercms-ruby'
 
 # If you added the Heroku Butter add-on, ENV["BUTTER_TOKEN"] will be defined.
 # Otherwise, grab your token at https://buttercms.com/profile/ and set it below
-ButterCMS::api_token = 'bd08588751a4aa85a24311b22d26fec21be3979c'
+ButterCMS::api_token = '01743179436109c0c755da1924b85c09ece380d9'
 
 # Test mode can be used to setup a staging website for previewing content or for testing content during local development.
 # ButterCMS::test_mode = true
